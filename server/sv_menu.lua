a3d7d325a672f1e6e1e8a08b234811a4460c007b
Bombay <72895600+BombayV@users.noreply.github.com>
2021-08-07 13:58:43 -0500

    fix(server): Change string into number

--- server/sv_menu.lua
+++ server/sv_menu.lua
@@ -5,8 +5,8 @@ RegisterNetEvent('ev:setTeleportData', function(isOpen, x, y, z, heading)
         if GetVehiclePedIsIn(ped, false) ~= 0 then
             ped = GetVehiclePedIsIn(GetPlayerPed(source), false)
         end
-        SetEntityCoords(ped, x, y, z, false, false, false, true)
-        SetEntityHeading(ped, heading)
+        SetEntityCoords(ped, tonumber(x), tonumber(y), tonumber(z), false, false, false, true)
+        SetEntityHeading(ped, tonumber(heading))
     else
         print('Possible hacker: ' .. tostring(source))
     end
